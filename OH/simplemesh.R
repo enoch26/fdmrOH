dab2c9a621f1817f9bc85cde0af6c21c9752da79
enoch26 <enochsuen26@gmail.com>
2024-03-26 15:12:20 +0000

    update mesh

--- OH/simplemesh.R
+++ OH/simplemesh.R
@@ -14,6 +14,13 @@ ocean_crs <- fm_crs(ocean_sf)
 fm_crs(ocean_sf) <- NULL
 ocean_sf2 <- st_union(st_buffer(ocean_sf, 1))
 fm_crs(ocean_sf) <- ocean_crs
+ocean_sp <- as_Spatial(ocean_sf)
+# ocean_sp <- sp::as(st_geometry(ocean_sf), "Spatial")
+
+sp::coordinates(ocean_sp) <- c("LONG", "LAT")
+fdmr::mesh_builder(spatial_data = ocean_sp,
+                   x_coord = "LONG",
+                   y_coord = "lat")
 
 ocean_bnd <- st_cast(
   st_sf(geometry = fm_nonconvex_hull(ocean_sf2, 5)),
@@ -24,10 +31,12 @@ ggplot() + geom_sf(data=ocean_sf2) + geom_sf(ocean_bnd)
 
 boundary <- fm_as_segm_list(list(ocean_sf2, ocean_bnd))
 
+
+
 mesh <- fm_mesh_2d_inla(boundary = boundary,
                         max.edge = c(2, 3),
                         offset = c(pi, pi))
 
-ggplot() + geom_sf(data=ocean_sf2) + gg(mesh)
-ggplot() + geom_sf(data=oceans.sf)
+ggplot() + geom_sf(data = ocean_sf2) + gg(mesh)
+ggplot() + geom_sf(data = oceans.sf)
 
