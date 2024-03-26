ce688ee041f5bc7a8c65c855833006f2cd7bd4c1
enoch26 <enochsuen26@gmail.com>
2024-03-26 14:28:48 +0000

    mesh update

--- OH/simplemesh.R
+++ OH/simplemesh.R
@@ -12,13 +12,19 @@ ocean_sf <-
 ocean_crs <- fm_crs(ocean_sf) 
 fm_crs(ocean_sf) <- NULL
 ocean_sf2 <- st_union(st_buffer(ocean_sf, 1))
+ocean_bnd <- st_cast(
+  st_sf(geometry = fm_nonconvex_hull(ocean_sf2, 1)),
+  "POLYGON"
+)
 
-ggplot() + geom_sf(data=ocean_sf2)
+ggplot() + geom_sf(data=ocean_sf2) + geom_sf(ocean_bnd)
 
-ocean_bnd <- 
-
-mesh <- fm_mesh_2d(boundary = ocean_sf2, max_edge)
-
-ggplot() + geom_sf(data=oceans.sf)
+plot(ocean_sf2)
+plot(ocean_bnd)  
+mesh <- fm_mesh_2d_inla(boundary = ocean_sf2,
+                        max.edge = 2,
+                        offset = c(pi, pi))
 
 ggplot() + geom_sf(data=ocean_sf2) + gg(mesh)
+ggplot() + geom_sf(data=oceans.sf)
+
