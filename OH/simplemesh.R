6cd8794b378810d393c103dced612e250b39727d
enoch26 <enochsuen26@gmail.com>
2024-03-26 15:13:36 +0000

    larger buffer

--- OH/simplemesh.R
+++ OH/simplemesh.R
@@ -12,13 +12,13 @@ ocean_sf <-
 # ocean_sf <- ocean_sf[as.numeric(st_area(ocean_sf)) > 50000]
 ocean_crs <- fm_crs(ocean_sf) 
 fm_crs(ocean_sf) <- NULL
-ocean_sf2 <- st_union(st_buffer(ocean_sf, 1))
+ocean_sf2 <- st_union(st_buffer(ocean_sf, 2))
 fm_crs(ocean_sf) <- ocean_crs
 ocean_sp <- as_Spatial(ocean_sf)
 # ocean_sp <- sp::as(st_geometry(ocean_sf), "Spatial")
 
 sp::coordinates(ocean_sp) <- c("LONG", "LAT")
-fdmr::mesh_builder(spatial_data = ocean_sp,
+fdmr::mesh_builder(spatial_data = ocean_sf,
                    x_coord = "LONG",
                    y_coord = "lat")
 
@@ -35,7 +35,8 @@ boundary <- fm_as_segm_list(list(ocean_sf2, ocean_bnd))
 
 mesh <- fm_mesh_2d_inla(boundary = boundary,
                         max.edge = c(2, 3),
-                        offset = c(pi, pi))
+                        offset = c(pi, pi),
+                        cutoff = 1)
 
 ggplot() + geom_sf(data = ocean_sf2) + gg(mesh)
 ggplot() + geom_sf(data = oceans.sf)
